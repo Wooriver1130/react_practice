689d112a42bdab2cbef61d547153176344645a10
=aphel <dldnrkdqkqh@gmail.com>
2024-11-19 22:02:44 +0900

    연습코드 11/19 오후

---
+++ src/components/Step05/Article05.jsx
@@ -0,0 +1,10 @@
+
+function Article05(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Article05;
---
+++ src/components/Step05/ArticleList05.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ArticleList05(props) {
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default ArticleList05;
